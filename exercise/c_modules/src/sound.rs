4144ecad935247a579aeb389a44653a13eeaa4bb
LTC-117 <lucastentoni@gmail.com>
2025-11-02 19:06:32 -0300

    [Implementing the modules exercise (simple implementation only)]

---
+++ exercise/c_modules/src/sound.rs
@@ -0,0 +1,21 @@
+// 2. Create a library module named `sound` and move the animal functions into it.
+//
+// - In your lib.rs file add the line `pub mod sound;`
+// - Create a `src/sound.rs` file for your module
+// - Move the `dog`, `cat`, and `fox` functions into sound.rs
+// - Make the functions public by adding the `pub` keyword in front of them
+// - Add a `use` statement to bring the `sound` module into scope.
+// - Change the function calls to access the functions through the `sound` module.
+//   For example: sound::dog()
+
+pub fn dog() {
+    println!("Dog goes WOOF!");
+}
+
+pub fn cat() {
+    println!("Cat goes MEOW!");
+}
+
+pub fn fox() {
+    println!("What does the fox say???");
+}
