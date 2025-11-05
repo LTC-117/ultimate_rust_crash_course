28f29569ecac1ab8c23c980ab0c0be95534c39cb
LTC-117 <lucastentoni@gmail.com>
2025-11-05 18:15:21 -0300

    [Making needed changes]

--- exercise/c_modules/src/main.rs
+++ exercise/c_modules/src/main.rs
@@ -1,9 +1,4 @@
-use animal::{
-    FIRST,
-    SECOND,
-    THIRD,
-    sound,
-};
+use animal::prelude::*;
 
 
 fn main() {
@@ -16,14 +11,3 @@ fn main() {
     print!("Listening to animal {}: ", THIRD);
     sound::wild::fox();
 }
-
-
-// Challenge 2
-//
-// Create an `animal::prelude` module which re-exports all of the constants and functions of the
-// library. (A real library would only re-export the most commonly-used items in its prelude.)
-//
-// Change your `use` statement(s) in main.rs to just `use animal::prelude::*`
-//
-// Hint: You will need `pub use` to re-export an item, for more details see:
-// https://doc.rust-lang.org/reference/items/use-declarations.html#use-visibility
